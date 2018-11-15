2a66947cec591c8f8bce2ac5d48e2f7cbda84555
Oliver Chang <ollieskywalker@gmail.com>
2018-11-14 21:38:39 -0800

    Initial Commit

---
+++ plot.py
@@ -0,0 +1,17 @@
+"""
+Disease Analysis Project
+By Oliver Chang
+Pomona '22
+11/14/2018
+"""
+
+import matplotlib as plt
+import pandas as pd
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()

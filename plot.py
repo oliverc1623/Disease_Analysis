03ca486fe24253c55003f56ba6edc1cdb95f8907
Oliver Chang <ollieskywalker@gmail.com>
2018-11-15 23:16:32 -0800

    Working menu option

--- plot.py
+++ plot.py
@@ -10,7 +10,9 @@ from tkinter import *
 from read_data import *
 
 df = return_data_frame()
+print(df.columns)
 topic_options = (return_topics(df))
+
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -22,21 +24,28 @@ class Window(Frame):
         self.master.title("Disease Analysis Application")
         self.pack(fill=BOTH, expand=1)
 
+        # Tkinter Variable
+        self.default_option = StringVar(root)
+        self.default_option.set("Cancer")  # set the default option
+
         # Welcome Label
         welcome_label = Label(self, text="Welcome to Disease Data Analysis. Pick a disease you'd like to see plotted.")
         welcome_label.pack()
 
         # Option Menu
-        default_option = StringVar(root)
-        # Dictionary with options
-        default_option.set("Cancer")  # set the default option
+        topic_option = OptionMenu(self, self.default_option, *topic_options)
+        topic_option.place(x=0, y=20)
 
-        topic_option = OptionMenu(self, default_option, *topic_options)
-        topic_option.pack()
+        # Run Button
+        run_button = Button(self, text="Run", command=self.run_analysis)
+        run_button.pack()
 
         # Quit Button
-        # quit_button = Button(self, text="Quit", command=self.client_exit)
-        # quit_button.place(x=0, y=0)
+        quit_button = Button(self, text="Quit", command=self.client_exit)
+        quit_button.place(x=0, y=0)
+
+    def run_analysis(self):
+        print(self.default_option.get())
 
     def client_exit(self):
         exit()
